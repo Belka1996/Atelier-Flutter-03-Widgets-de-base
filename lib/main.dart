3502fb769bf8a46bcb63f363a39c0b58ac1e7c11
Belka1996 <medjkounebelkacem2019@gmail.com>
2025-11-15 02:58:57 -0500

    Atelier Flutter 3 Étape 3 ok

--- lib/main.dart
+++ lib/main.dart
@@ -17,8 +17,32 @@ class MainApp extends StatelessWidget {
           centerTitle: true,
           backgroundColor: Colors.blueAccent,
         ),
-        body: Center(
-          child: Text('Bienvenue dans cet atelier!'),
+        body: Container(
+          alignment: Alignment.topCenter,
+          child: Column(
+            children: [
+              SizedBox(height: 20),
+              Text(
+                'Bienvenue dans cet atelier !',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                  color: const Color.fromARGB(255, 255, 68, 236),
+                ),
+              ),
+              SizedBox(height: 20),
+              Image.asset(
+                'assets/drapeau-amazigh-1-1.jpg',
+                width: 250,
+              ),
+              SizedBox(height: 20),
+              Text(
+                'Découvrez comment construire une interface simple et élégante(Personnalisée par Belkacem)',
+                textAlign: TextAlign.center,
+                style: TextStyle(fontSize: 16),
+              ),
+            ],
+          ),
         ),
       ),
     );
